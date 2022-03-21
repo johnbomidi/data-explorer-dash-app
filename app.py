45124c3b725e3519c5c6e19bddef033f4ba149c1
bomidi <john.bomidi@gmail.com>
2022-03-20 19:03:16 -0500

    app.py clean up

--- app.py
+++ app.py
@@ -7,31 +7,11 @@ from _app.callback import register_callbacks
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-
-# Server definition
-
 app = Dash(prevent_initial_callbacks=True, 
             external_stylesheets=external_stylesheets
             )
 
-# HEADER
-# ======
-
-
-# COMPONENTS
-# ==========
-
-# Your components go here.
-
-
-# INTERACTION
-# ===========
-
-# Your interaction goes here.
 register_callbacks(app)
-
-# APP LAYOUT
-# ==========
 
 app.layout = serve_layout()
 
